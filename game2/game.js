ea6180fb10d38e94a2122623cf61e5fe06c98739
Fábio França <fabiofranca593@gmail.com>
2022-04-17 17:18:54 -0300

    Update game2

--- game2/game.js
+++ game2/game.js
@@ -7,8 +7,12 @@ const OBSTACLES = 50
 let LEFT = false
 let RIGHT = false
 let STOPPED = false
+let BULLET_LEFT = false
+let BULLET_RIGHT = false
+let BULLET_UP = false
 let IS_ON_TOP_OF_OBSTACLE = false
-let POINT = null
+const points = []
+const bullets = []
 const obstacles = []
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
@@ -28,8 +32,9 @@ const sun = {
     ctx.closePath()
   },
   move: function () {
-    if (LEFT) this.x += person.speed / 20
-    if (RIGHT) this.x -= person.speed / 20
+    const { length } = obstacles
+    if (LEFT) this.x += person.speed / (length * 0.75)
+    if (RIGHT) this.x -= person.speed / (length * 0.75)
   },
 }
 
@@ -38,6 +43,7 @@ const person = {
   y: 0,
   size: 50,
   speed: 8,
+  score: 0,
   jumping: false,
   color: '#0095DD',
   jump_speed: 0,
@@ -48,8 +54,10 @@ const person = {
     this.jump_speed = 0
   },
   draw: function () {
+    ctx.beginPath()
     ctx.fillStyle = this.color
     ctx.fillRect(this.x, this.y, this.size, this.size)
+    ctx.closePath()
   },
   update: function () {
     if (this.jumping && !IS_ON_TOP_OF_OBSTACLE) {
@@ -95,17 +103,25 @@ function renderSoil() {
   ctx.closePath()
 }
 
+/**
+ *
+ * @param {number} width
+ * @returns {number}
+ */
+function getRandomX(width) {
+  return CENTER_X / 2 + Math.random() * CENTER_X + width
+}
+
 const createObstacle = () => {
   const width = Math.random() * 400 + 50
   const height = Math.random() * 120 + 40
-  function getRandomX() {
-    return CENTER_X + Math.random() * CENTER_X + width
+  const lastObstacle = obstacles[obstacles.length - 1] || {
+    x: getRandomX(width),
   }
-  const lastObstacle = obstacles[obstacles.length - 1] || { x: getRandomX() }
   const obstacle = {
     width,
     height: Math.random() > 0.5 ? height / 2 : height,
-    x: getRandomX() + lastObstacle.x,
+    x: getRandomX(width) + lastObstacle.x,
     y: HEIGHT - SOIL_HEIGHT - height,
     color: '#04d361',
     draw: function () {
@@ -125,9 +141,13 @@ const createObstacle = () => {
 
 function move(key, value) {
   const _value = value ? key : null
-  if (key === 'ArrowUp') return person.jump()
-  if (key === 'ArrowLeft') return (LEFT = _value)
-  if (key === 'ArrowRight') return (RIGHT = _value)
+  if (key === 'w') return person.jump()
+  if (key === 'a') return (LEFT = _value)
+  if (key === 'd') return (RIGHT = _value)
+  if (key === 'ArrowLeft') BULLET_LEFT = _value
+  if (key === 'ArrowRight') BULLET_RIGHT = _value
+  if (key === 'ArrowUp') BULLET_UP = _value
+  if (BULLET_LEFT || BULLET_RIGHT || BULLET_UP) createBullet()
 }
 
 document.onkeydown = function (e) {
@@ -157,6 +177,7 @@ function renderInfo() {
   )
   ctx.fillText(`gravity: ${GRAVITY}`, 10, 140)
   ctx.fillText(`obstacles: ${OBSTACLES}`, 10, 155)
+  ctx.fillText(`score: ${person.score}`, 10, 170)
 }
 
 function isOnTopObstacle(person, obstacle) {
@@ -175,16 +196,24 @@ function leftCollision(person, obstacle) {
   return (
     posX >= obstacle.x &&
     posX < obstacle.x + obstacle.width &&
-    person.y >= obstacle.y &&
-    person.y <= obstacle.y + obstacle.height
+    person.y + person.size > obstacle.y &&
+    person.y < obstacle.y + obstacle.height
   )
 }
 function rightCollision(person, obstacle) {
   return (
     person.x <= obstacle.x + obstacle.width &&
-    person.x + person.size > obstacle.x &&
-    person.y >= obstacle.y &&
+    person.x > obstacle.x - person.size &&
+    person.y + person.size >= obstacle.y &&
     person.y <= obstacle.y + obstacle.height
+  )
+}
+
+function isUnderObstacle(obstacle) {
+  return (
+    person.y > obstacle.y + obstacle.height &&
+    person.x + person.size > obstacle.x &&
+    person.x < obstacle.x + obstacle.width
   )
 }
 
@@ -195,6 +224,7 @@ function checkObstacleCollision() {
     obstacleIndex++
   ) {
     const obstacle = obstacles[obstacleIndex]
+    console.log(rightCollision(person, obstacle))
     if (isOnTopObstacle(person, obstacle)) {
       IS_ON_TOP_OF_OBSTACLE = true
       person.y = obstacle.y - person.size
@@ -203,8 +233,7 @@ function checkObstacleCollision() {
     }
     if (leftCollision(person, obstacle)) {
       person.x = obstacle.x - person.size
-    }
-    if (rightCollision(person, obstacle)) {
+    } else if (rightCollision(person, obstacle)) {
       person.x = obstacle.x + obstacle.width
     }
     if (IS_ON_TOP_OF_OBSTACLE) {
@@ -215,11 +244,13 @@ function checkObstacleCollision() {
 
 function createPoint() {
   const minY = HEIGHT * 0.45
+  const size = 14
   const value = minY - SOIL_HEIGHT - 50
+  const lastPoint = points[points.length - 1] || { x: getRandomX(size) }
   const randomValue = Math.random() * value
   const point = {
-    size: 10,
-    x: WIDTH,
+    size,
+    x: lastPoint.x + getRandomX(size),
     y: minY + randomValue,
     color: '#ff5555',
     draw: function () {
@@ -255,11 +286,95 @@ function createObstacles(n) {
   }
 }
 
+function createPoints(n) {
+  for (let i = 0; i < n; i++) {
+    points.push(createPoint())
+  }
+}
+
+function checkPointCollision() {
+  for (let pointIndex = 0; pointIndex < points.length; pointIndex++) {
+    const point = points[pointIndex]
+    if (
+      point.x + point.size >= person.x &&
+      point.x <= person.x + person.size &&
+      point.y + point.size >= person.y &&
+      point.y <= person.y + person.size
+    ) {
+      points.splice(pointIndex, 1)
+      person.score += 1
+    }
+  }
+}
+
+function renderPoints() {
+  for (let pointIndex = 0; pointIndex < points.length; pointIndex++) {
+    const point = points[pointIndex]
+    point.move()
+    point.draw()
+  }
+}
+
+function createEnemie() {
+  ctx.beginPath()
+  ctx.move
+  ctx.closePath()
+}
+
+function createBullet() {
+  const size = 10
+  const x =
+    BULLET_LEFT || BULLET_RIGHT
+      ? BULLET_LEFT
+        ? person.x
+        : person.x + person.size - size
+      : person.x + person.size / 2 - size / 2
+  const y = BULLET_UP ? person.y : person.y + person.size / 2 - size / 2
+  const speed = 10
+  const vX = BULLET_LEFT || BULLET_RIGHT ? (BULLET_LEFT ? -speed : speed) : 0
+  const vY = BULLET_UP ? -speed : 0
+  const bullet = {
+    x,
+    y,
+    size,
+    color: '#000000',
+    speed: 10,
+    vX,
+    vY,
+    draw: function () {
+      ctx.beginPath()
+      ctx.fillStyle = this.color
+      ctx.rect(this.x, this.y, this.size, this.size)
+      ctx.fill()
+      ctx.closePath()
+    },
+    move: function () {
+      this.y += this.vY
+      this.x += this.vX
+    },
+  }
+  bullets.push(bullet)
+}
+
+function renderBullets() {
+  for (let bulletIndex = 0; bulletIndex < bullets.length; bulletIndex++) {
+    const bullet = bullets[bulletIndex]
+    if (bullet.x < 0 || bullet.x > WIDTH) {
+      bullets.splice(bulletIndex, 1)
+    } else {
+      bullet.move()
+      bullet.draw()
+    }
+  }
+}
+
 function main() {
   person.init()
   createObstacles(OBSTACLES)
+  createPoints(OBSTACLES)
   function loop() {
     person.update()
+    checkPointCollision()
     checkObstacleCollision()
     ctx.clearRect(0, 0, WIDTH, HEIGHT)
     renderObstacles()
@@ -267,16 +382,11 @@ function main() {
     sun.draw()
     if (!STOPPED) sun.move()
     person.draw()
-    if (POINT) {
-      POINT.draw()
-      POINT.move()
-    }
+    renderPoints()
+    renderBullets()
     renderInfo()
     requestAnimationFrame(loop)
   }
-  setInterval(() => {
-    POINT = createPoint()
-  }, [2000])
   loop()
 }
 
