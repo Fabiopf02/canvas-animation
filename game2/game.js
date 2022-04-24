ace987e71cad7e5b6d9bbc8d6faf2abdbe6fe82d
Fábio França <fabiofranca593@gmail.com>
2022-04-24 00:23:31 -0300

    Update game2

--- game2/game.js
+++ game2/game.js
@@ -4,6 +4,7 @@ const CENTER_X = WIDTH / 2
 const SOIL_HEIGHT = 100
 const GRAVITY = 10
 const OBSTACLES = 50
+const TREES = 200
 let LEFT = false
 let RIGHT = false
 let STOPPED = false
@@ -15,6 +16,19 @@ let IS_UNDER_OBSTACLE = false
 const points = []
 const bullets = []
 const obstacles = []
+const enemies = []
+const obstaclesWithRotation = []
+const trees = []
+const colors = {
+  person: '#0095DD',
+  sun: '#ffd700',
+  obstacle: '#7159c1',
+  enemie: '#ff4444',
+  point: '#ff6666',
+  bullet: '#000000',
+  tree: '#069647',
+  obstacleWithRotation: '#9b49c1',
+}
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 canvas.width = WIDTH
@@ -24,7 +38,7 @@ const sun = {
   x: WIDTH / 1.2,
   y: HEIGHT / 6,
   radius: 70,
-  color: '#ffd700',
+  color: colors.sun,
   draw: function () {
     ctx.beginPath()
     ctx.fillStyle = this.color
@@ -46,7 +60,7 @@ const person = {
   speed: 8,
   score: 0,
   jumping: false,
-  color: '#0095DD',
+  color: colors.person,
   jump_speed: 0,
   jump_time: 0,
   current_jump_speed: 0,
@@ -57,7 +71,19 @@ const person = {
   draw: function () {
     ctx.beginPath()
     ctx.fillStyle = this.color
+    ctx.strokeStyle = '#ffffff'
+    ctx.lineWidth = 10
     ctx.fillRect(this.x, this.y, this.size, this.size)
+    ctx.arc(
+      this.x + this.size / 2,
+      this.y + this.size / 2,
+      this.size / 5,
+      0,
+      Math.PI * 2,
+      false
+    )
+    ctx.stroke()
+    ctx.fill()
     ctx.closePath()
   },
   update: function () {
@@ -106,7 +132,7 @@ function getVelocity(v0, t) {
 
 function renderSoil() {
   ctx.beginPath()
-  ctx.fillStyle = '#04d361'
+  ctx.fillStyle = colors.tree
   ctx.rect(0, HEIGHT - SOIL_HEIGHT, WIDTH, SOIL_HEIGHT)
   ctx.fill()
   ctx.closePath()
@@ -132,7 +158,7 @@ const createObstacle = () => {
     height: Math.random() > 0.5 ? height / 2 : height,
     x: getRandomX(width) + lastObstacle.x,
     y: HEIGHT - SOIL_HEIGHT - height,
-    color: '#04d361',
+    color: colors.obstacle,
     draw: function () {
       ctx.beginPath()
       ctx.fillStyle = this.color
@@ -150,6 +176,7 @@ const createObstacle = () => {
 
 function move(key, value) {
   const _value = value ? key : null
+  if (key === 't') document.querySelector('body').style.background = '#000'
   if (key === 'w') return person.jump()
   if (key === 'a') return (LEFT = _value)
   if (key === 'd') return (RIGHT = _value)
@@ -271,7 +298,7 @@ function createPoint() {
     size,
     x: lastPoint.x + getRandomX(size),
     y: minY + randomValue,
-    color: '#ff5555',
+    color: colors.point,
     draw: function () {
       ctx.beginPath()
       ctx.fillStyle = this.color
@@ -371,7 +398,7 @@ function createBullet() {
     x,
     y,
     size,
-    color: '#000000',
+    color: colors.bullet,
     speed: 10,
     vX,
     vY,
@@ -410,23 +437,241 @@ function renderBullets() {
   }
 }
 
+function createEnemie() {
+  const size = 20
+  const lastEnemie = enemies[enemies.length - 1] || { x: getRandomX(size) }
+  const x = lastEnemie.x + getRandomX(size)
+  const y = HEIGHT - SOIL_HEIGHT - size
+  const speed = Math.random() * 2 + 1
+  const vX = speed
+  const enemie = {
+    x,
+    y,
+    size,
+    color: colors.enemie,
+    speed,
+    vX,
+    draw: function () {
+      ctx.beginPath()
+      ctx.fillStyle = this.color
+      ctx.rect(this.x, this.y, this.size, this.size)
+      ctx.fill()
+      ctx.closePath()
+    },
+    move: function () {
+      this.x += this.vX
+    },
+  }
+  enemies.push(enemie)
+}
+
+function createEnemies(n) {
+  for (let i = 0; i < n; i++) {
+    createEnemie()
+  }
+}
+
+function renderEnemies() {
+  for (let enemieIndex = 0; enemieIndex < enemies.length; enemieIndex++) {
+    const enemie = enemies[enemieIndex]
+    enemie.move()
+    enemie.draw()
+  }
+}
+
+function checkEnemieObstacleCollision() {
+  for (let enemieIndex = 0; enemieIndex < enemies.length; enemieIndex++) {
+    const enemie = enemies[enemieIndex]
+    for (
+      let obstacleIndex = 0;
+      obstacleIndex < obstacles.length;
+      obstacleIndex++
+    ) {
+      const obstacle = obstacles[obstacleIndex]
+      if (
+        enemie.x + enemie.size >= obstacle.x &&
+        enemie.x <= obstacle.x + obstacle.width &&
+        enemie.y + enemie.size >= obstacle.y &&
+        enemie.y <= obstacle.y + obstacle.height
+      ) {
+        enemie.vX *= -1
+      }
+    }
+  }
+}
+
+function getDistance(x1, y1, x2, y2) {
+  const dx = x2 - x1
+  const dy = y2 - y1
+  return Math.sqrt(dx * dx + dy * dy)
+}
+
+function createObstacleWithRotation() {
+  const radius = Math.random() * 15 + 20
+  const lastObstacleWithRotation = obstaclesWithRotation[
+    obstaclesWithRotation.length - 1
+  ] || { x: getRandomX(radius) }
+  const AREA = HEIGHT - SOIL_HEIGHT
+  const centerY = Math.random() * (AREA / 2) + AREA / 2 - radius
+  const centerX = lastObstacleWithRotation.x + getRandomX(radius)
+  const d = getDistance(centerX, centerY, centerX, AREA - radius)
+  let y = centerY + d
+  let x = centerX + d + radius
+  const obstacle = {
+    x,
+    y,
+    centerX,
+    centerY,
+    speed: Math.random() * 3 + 1,
+    angle: 0,
+    color: colors.obstacleWithRotation,
+    radius,
+    d,
+    draw: function () {
+      ctx.beginPath()
+      // ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+      ctx.rect(this.x, this.y, this.radius, this.radius)
+      ctx.fillStyle = this.color
+      ctx.moveTo(this.centerX, this.centerY)
+      ctx.lineTo(this.x + this.radius / 2, this.y + this.radius / 2)
+      ctx.lineWidth = 1
+      ctx.strokeStyle = this.color
+      ctx.rect(this.centerX - 5, this.centerY - 5, 10, this.d + radius + 5)
+      ctx.stroke()
+      ctx.fill()
+      ctx.closePath()
+    },
+    move: function () {
+      const newX = this.d * Math.cos(this.angle * (Math.PI / 180))
+      const newY = this.d * Math.sin(this.angle * (Math.PI / 180))
+      this.x = this.centerX + newX + Math.cos(this.angle * (Math.PI / 180))
+      this.y = this.centerY + newY + Math.sin(this.angle * (Math.PI / 180))
+      this.angle += this.speed
+      if (LEFT) this.centerX += person.speed
+      if (RIGHT) this.centerX -= person.speed
+    },
+  }
+  obstaclesWithRotation.push(obstacle)
+  return obstacle
+}
+
+function createObstaclesWithRotation(n) {
+  for (let i = 0; i < n; i++) {
+    createObstacleWithRotation()
+  }
+}
+
+function renderObstaclesWithRotation() {
+  for (
+    let obstacleWithRotationIndex = 0;
+    obstacleWithRotationIndex < obstaclesWithRotation.length;
+    obstacleWithRotationIndex++
+  ) {
+    const obstacleWithRotation =
+      obstaclesWithRotation[obstacleWithRotationIndex]
+    obstacleWithRotation.move()
+    obstacleWithRotation.draw()
+  }
+}
+
+function checkObstacleWithRotationCollisionWithPerson() {
+  for (
+    let obstacleWithRotationIndex = 0;
+    obstacleWithRotationIndex < obstaclesWithRotation.length;
+    obstacleWithRotationIndex++
+  ) {
+    const obstacleWithRotation =
+      obstaclesWithRotation[obstacleWithRotationIndex]
+    if (
+      person.x + person.size >= obstacleWithRotation.x &&
+      person.x <= obstacleWithRotation.x + obstacleWithRotation.radius &&
+      person.y + person.size >= obstacleWithRotation.y &&
+      person.y <= obstacleWithRotation.y + obstacleWithRotation.radius
+    ) {
+      console.log('game over')
+    }
+  }
+}
+
+function createTree() {
+  const width = Math.random() * 200 + 50
+  const lastTree = trees[trees.length - 1] || { x: getRandomX(width) / 3 }
+  const x = lastTree.x + getRandomX(width) / 5
+  const bodyWidth = Math.random() * (width * 0.2) + width * 0.2
+  const y = Math.random() * ((HEIGHT - SOIL_HEIGHT) / 2) + 80
+  const height = Math.random() * (HEIGHT - y - SOIL_HEIGHT - 150) + 150
+  const tree = {
+    x,
+    y,
+    width,
+    height,
+    bodyWidth,
+    draw: function () {
+      ctx.beginPath()
+      ctx.fillStyle = colors.tree
+      ctx.moveTo(this.x, this.y)
+      ctx.lineTo(this.x - this.width / 2, this.y + this.height)
+      ctx.lineTo(this.x + this.width / 2, this.y + this.height)
+      ctx.fill()
+      ctx.closePath()
+      ctx.beginPath()
+      ctx.fillStyle = '#4f3b12'
+      ctx.rect(
+        this.x - this.bodyWidth / 2,
+        this.y + this.height,
+        this.bodyWidth,
+        HEIGHT - this.y - this.height - SOIL_HEIGHT
+      )
+      ctx.fill()
+      ctx.closePath()
+    },
+    move: function () {
+      if (LEFT) this.x += person.speed * 0.3
+      if (RIGHT) this.x -= person.speed * 0.3
+    },
+  }
+  trees.push(tree)
+}
+
+function createTrees(n) {
+  for (let i = 0; i < n; i++) {
+    createTree()
+  }
+}
+
+function renderTrees() {
+  for (let treeIndex = 0; treeIndex < trees.length; treeIndex++) {
+    const tree = trees[treeIndex]
+    tree.move()
+    tree.draw()
+  }
+}
+
 function main() {
   person.init()
   createObstacles(OBSTACLES)
   createPoints(OBSTACLES)
+  createEnemies(OBSTACLES)
+  createObstaclesWithRotation(OBSTACLES)
+  createTrees(TREES)
   function loop() {
     person.update()
     checkPointCollision()
     checkObstacleCollision()
     checkBulletObstacleCollision()
+    checkEnemieObstacleCollision()
+    checkObstacleWithRotationCollisionWithPerson()
     ctx.clearRect(0, 0, WIDTH, HEIGHT)
-    renderObstacles()
-    renderSoil()
     sun.draw()
     if (!STOPPED) sun.move()
+    renderTrees()
+    renderSoil()
+    renderObstacles()
     person.draw()
+    renderEnemies()
     renderPoints()
     renderBullets()
+    renderObstaclesWithRotation()
     renderInfo()
     requestAnimationFrame(loop)
   }
