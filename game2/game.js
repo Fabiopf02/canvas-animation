91032d9b43d656ba5b033dfec9ebb26d4cae463b
fabiofranca02 <fabio.franca@algoritme.com.br>
2022-04-26 13:54:44 -0300

    Update game2

--- game2/game.js
+++ game2/game.js
@@ -5,14 +5,17 @@ const SOIL_HEIGHT = 100
 const GRAVITY = 10
 const OBSTACLES = 50
 const TREES = 200
+let GAME_SPEED = 10
 let LEFT = false
-let RIGHT = false
+let RIGHT = true
 let STOPPED = false
 let BULLET_LEFT = false
 let BULLET_RIGHT = false
 let BULLET_UP = false
 let IS_ON_TOP_OF_OBSTACLE = false
 let IS_UNDER_OBSTACLE = false
+let GAME_OVER = false
+let PERSON_COLLISION = true
 const points = []
 const bullets = []
 const obstacles = []
@@ -26,18 +29,40 @@ const colors = {
   enemie: '#ff4444',
   point: '#ff6666',
   bullet: '#000000',
+  night: '#000000',
+  day: 'rgb(163, 204, 238)',
+  moom: '#ffffff',
   tree: '#069647',
   obstacleWithRotation: '#9b49c1',
+}
+const tSky = OBSTACLES.length * WIDTH
+const sky = {
+  r: { t: 163 / tSky, v: 163 },
+  g: { t: 204 / tSky, v: 204 },
+  b: { t: 238 / tSky, v: 238 },
 }
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 canvas.width = WIDTH
 canvas.height = HEIGHT
+function getColor() {
+  if (sun.state === 'day') {
+    sky.r.v -= sky.r.t
+    sky.g.v -= sky.g.t
+    sky.b.v -= sky.b.t
+  } else {
+    sky.r.v += sky.r.t
+    sky.g.v += sky.g.t
+    sky.b.v += sky.b.t
+  }
+  return `rgb(${sky.r.v}, ${sky.g.v}, ${sky.b.v})`
+}
 
 const sun = {
   x: WIDTH / 1.2,
   y: HEIGHT / 6,
   radius: 70,
+  state: 'day',
   color: colors.sun,
   draw: function () {
     ctx.beginPath()
@@ -48,8 +73,8 @@ const sun = {
   },
   move: function () {
     const { length } = obstacles
-    if (LEFT) this.x += person.speed / (length * 0.75)
-    if (RIGHT) this.x -= person.speed / (length * 0.75)
+    if (LEFT) this.x += GAME_SPEED / (length * 0.75)
+    if (RIGHT) this.x -= GAME_SPEED / (length * 0.75)
   },
 }
 
@@ -57,7 +82,6 @@ const person = {
   x: 10,
   y: 0,
   size: 50,
-  speed: 8,
   score: 0,
   jumping: false,
   color: colors.person,
@@ -106,11 +130,11 @@ const person = {
     }
     if (LEFT) {
       if (this.x <= 0) return
-      this.x -= this.speed
+      this.x -= GAME_SPEED
     } else if (RIGHT) {
-      if (this.x + this.size >= CENTER_X * 0.8 || this.x + this.size >= WIDTH)
+      if (this.x + this.size >= CENTER_X * 0.4 || this.x + this.size >= WIDTH)
         return
-      this.x += this.speed
+      this.x += GAME_SPEED
     }
   },
   jump: function () {
@@ -120,7 +144,7 @@ const person = {
     }
     if (this.jumping === false) {
       this.jump_time = 0
-      this.jump_speed = 25
+      this.jump_speed = 30
       this.jumping = true
     }
   },
@@ -167,8 +191,8 @@ const createObstacle = () => {
       ctx.closePath()
     },
     move: function () {
-      if (LEFT) this.x += person.speed
-      if (RIGHT) this.x -= person.speed
+      if (LEFT) this.x += GAME_SPEED
+      if (RIGHT) this.x -= GAME_SPEED
     },
   }
   return obstacle
@@ -176,10 +200,9 @@ const createObstacle = () => {
 
 function move(key, value) {
   const _value = value ? key : null
-  if (key === 't') document.querySelector('body').style.background = '#000'
   if (key === 'w') return person.jump()
-  if (key === 'a') return (LEFT = _value)
-  if (key === 'd') return (RIGHT = _value)
+  // if (key === 'a') return (LEFT = _value)
+  // if (key === 'd') return (RIGHT = _value)
   if (key === 'ArrowLeft') BULLET_LEFT = _value
   if (key === 'ArrowRight') BULLET_RIGHT = _value
   if (key === 'ArrowUp') BULLET_UP = _value
@@ -214,6 +237,7 @@ function renderInfo() {
   ctx.fillText(`gravity: ${GRAVITY}`, 10, 140)
   ctx.fillText(`obstacles: ${OBSTACLES}`, 10, 155)
   ctx.fillText(`score: ${person.score}`, 10, 170)
+  ctx.fillText(`fame speed: ${GAME_SPEED}`, 10, 185)
 }
 
 function isOnTopObstacle(person, obstacle) {
@@ -307,8 +331,8 @@ function createPoint() {
       ctx.closePath()
     },
     move: function () {
-      if (LEFT) this.x += person.speed
-      if (RIGHT) this.x -= person.speed
+      if (LEFT) this.x += GAME_SPEED
+      if (RIGHT) this.x -= GAME_SPEED
     },
   }
   return point
@@ -391,7 +415,7 @@ function createBullet() {
         : person.x + person.size - size
       : person.x + person.size / 2 - size / 2
   const y = BULLET_UP ? person.y : person.y + person.size / 2 - size / 2
-  const speed = 10
+  const speed = GAME_SPEED * 1.1
   const vX = BULLET_LEFT || BULLET_RIGHT ? (BULLET_LEFT ? -speed : speed) : 0
   const vY = BULLET_UP ? -speed : 0
   const bullet = {
@@ -399,7 +423,7 @@ function createBullet() {
     y,
     size,
     color: colors.bullet,
-    speed: 10,
+    speed,
     vX,
     vY,
     time: 0,
@@ -438,7 +462,7 @@ function renderBullets() {
 }
 
 function createEnemie() {
-  const size = 20
+  const size = Math.random() * 15 + 25
   const lastEnemie = enemies[enemies.length - 1] || { x: getRandomX(size) }
   const x = lastEnemie.x + getRandomX(size)
   const y = HEIGHT - SOIL_HEIGHT - size
@@ -522,7 +546,7 @@ function createObstacleWithRotation() {
     y,
     centerX,
     centerY,
-    speed: Math.random() * 3 + 1,
+    speed: Math.random() * 3 + 1.5,
     angle: 0,
     color: colors.obstacleWithRotation,
     radius,
@@ -547,8 +571,8 @@ function createObstacleWithRotation() {
       this.x = this.centerX + newX + Math.cos(this.angle * (Math.PI / 180))
       this.y = this.centerY + newY + Math.sin(this.angle * (Math.PI / 180))
       this.angle += this.speed
-      if (LEFT) this.centerX += person.speed
-      if (RIGHT) this.centerX -= person.speed
+      if (LEFT) this.centerX += GAME_SPEED
+      if (RIGHT) this.centerX -= GAME_SPEED
     },
   }
   obstaclesWithRotation.push(obstacle)
@@ -574,6 +598,13 @@ function renderObstaclesWithRotation() {
   }
 }
 
+function changeTheme() {
+  document.querySelector('body').style.background =
+    sun.state === 'night' ? colors.night : colors.day
+  sun.color = sun.state === 'night' ? colors.moom : colors.sun
+  sun.x = WIDTH + sun.radius
+}
+
 function checkObstacleWithRotationCollisionWithPerson() {
   for (
     let obstacleWithRotationIndex = 0;
@@ -588,7 +619,7 @@ function checkObstacleWithRotationCollisionWithPerson() {
       person.y + person.size >= obstacleWithRotation.y &&
       person.y <= obstacleWithRotation.y + obstacleWithRotation.radius
     ) {
-      console.log('game over')
+      GAME_OVER = true
     }
   }
 }
@@ -626,8 +657,8 @@ function createTree() {
       ctx.closePath()
     },
     move: function () {
-      if (LEFT) this.x += person.speed * 0.3
-      if (RIGHT) this.x -= person.speed * 0.3
+      if (LEFT) this.x += GAME_SPEED * 0.3
+      if (RIGHT) this.x -= GAME_SPEED * 0.3
     },
   }
   trees.push(tree)
@@ -647,6 +678,48 @@ function renderTrees() {
   }
 }
 
+function checkBulletEnemiesCollision() {
+  for (let bulletIndex = 0; bulletIndex < bullets.length; bulletIndex++) {
+    const bullet = bullets[bulletIndex]
+    for (let enemyIndex = 0; enemyIndex < enemies.length; enemyIndex++) {
+      const enemy = enemies[enemyIndex]
+      if (
+        bullet.x + bullet.size >= enemy.x &&
+        bullet.x <= enemy.x + enemy.size &&
+        bullet.y + bullet.size >= enemy.y &&
+        bullet.y <= enemy.y + enemy.size
+      ) {
+        enemies.splice(enemyIndex, 1)
+        bullets.splice(bulletIndex, 1)
+        person.score += 1
+      }
+    }
+  }
+}
+
+function checkTheme() {
+  if (sun.x + sun.radius < 0) {
+    sun.state = sun.state === 'day' ? 'night' : 'day'
+    changeTheme()
+  }
+  const color = getColor()
+  document.querySelector('body').style.background = color
+}
+
+function checkPersonEnemiesCollision() {
+  for (let enemyIndex = 0; enemyIndex < enemies.length; enemyIndex++) {
+    const enemy = enemies[enemyIndex]
+    if (
+      person.x + person.size >= enemy.x &&
+      person.x <= enemy.x + enemy.size &&
+      person.y + person.size >= enemy.y &&
+      person.y <= enemy.y + enemy.size
+    ) {
+      GAME_OVER = true
+    }
+  }
+}
+
 function main() {
   person.init()
   createObstacles(OBSTACLES)
@@ -655,12 +728,19 @@ function main() {
   createObstaclesWithRotation(OBSTACLES)
   createTrees(TREES)
   function loop() {
+    if (GAME_OVER) return alert('GAME OVER')
+    GAME_SPEED += 0.001
+    checkTheme()
     person.update()
     checkPointCollision()
-    checkObstacleCollision()
+    if (PERSON_COLLISION) {
+      checkObstacleCollision()
+      checkObstacleWithRotationCollisionWithPerson()
+      checkPersonEnemiesCollision()
+    }
+    checkBulletEnemiesCollision()
     checkBulletObstacleCollision()
     checkEnemieObstacleCollision()
-    checkObstacleWithRotationCollisionWithPerson()
     ctx.clearRect(0, 0, WIDTH, HEIGHT)
     sun.draw()
     if (!STOPPED) sun.move()
